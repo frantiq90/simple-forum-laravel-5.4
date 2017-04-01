c4fd7c1f71a8a9790c30c961ec9c795c1a8bebd2
frantiq90 <oscar.kosela@gmail.com>
2017-04-01 19:41:15 +0000

    some error validation in create post form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,12 +46,16 @@
                         <li><a href="/threads">All Threads</a></li>
 
                         <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels<span class="caret"></span></a>
                           <ul class="dropdown-menu">
                             @foreach(App\Channel::all() as $channel)
                                 <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
                             @endforeach
                           </ul>
+                        </li>
+
+                        <li>
+                            <a href="/threads/create">New Thread</a>
                         </li>
 
                     </ul>
