0d1c0100d4bcfd72ce99bc5fa552d127ae52a9db
frantiq90 <oscar.kosela@gmail.com>
2017-04-01 10:01:14 +0000

    channel assigned to thread

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -9,7 +9,7 @@ class ThreadsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth')->except(['index', 'show']);
     }
     /**
      * Display a listing of the resource.
@@ -30,7 +30,7 @@ class ThreadsController extends Controller
      */
     public function create()
     {
-        //
+        return view('threads.create');
     }
 
     /**
@@ -43,6 +43,7 @@ class ThreadsController extends Controller
     {
         $thread = Thread::create([
             'user_id' => auth()->id(),
+            'channel_id' => request('channel_id'),
             'title' => request('title'),
             'body' => request('body')
         ]);
@@ -56,7 +57,7 @@ class ThreadsController extends Controller
      * @param  \App\Thread  $thread
      * @return \Illuminate\Http\Response
      */
-    public function show(Thread $thread)
+    public function show($channelId, Thread $thread)
     {
         return view('threads.show', compact('thread'));
     }
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,35 +2,37 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
 class CreateThreadsTest extends TestCase
 {
 	use DatabaseMigrations;
 
 	/** @test */
-	function guests_may_not_create_threads()
-	{
-		$this->expectException('Illuminate\Auth\AuthenticationException');
+	 function guests_may_not_create_threads()
+    {
+        $this->withExceptionHandling();
 
-		$thread = make('App\Thread');
+        $this->get('/threads/create')
+            ->assertRedirect('/login');
 
-		$this->post('/threads', $thread->toArray());
-	}
+            $this->post('/threads')
+                ->assertRedirect('/login');
+    }
+
     /** @test */
     function an_authenticated_user_can_create_new_forum_threads()
     {
-    	$this->singIn();
+        $this->signIn();
 
-    	$thread = make('App\Thread');
+        $thread = create('App\Thread');
 
-    	$this->post('/threads', $thread->toArray());
-
-    	$this->get($thread->path())
-    		->assertSee($thread->title)
-    		->assertSee($thread->body);
+        $this->post('/threads', $thread->toArray());
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
     }
+
+
 }
