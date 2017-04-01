be5f7050fca3c311e9fd78b3efedb01a8781b286
frantiq90 <oscar.kosela@gmail.com>
2017-04-01 11:05:04 +0000

    given some more validation

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -2,33 +2,36 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
 
-class ParticipateInForumTest extends TestCase
+class ParticipateInThreadsTest extends TestCase
 {
-	use DatabaseMigrations;
-
-	/** @test */
-    function unauthenticated_user_may_not_add_replies()
+    use DatabaseMigrations;
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
     {
-    	$this->withExceptionHandling()
+        $this->withExceptionHandling()
             ->post('/threads/some-channel/1/replies', [])
             ->assertRedirect('/login');
     }
-
     /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
     {
-    	$this->be($user = factory('App\User')->create());
-
-    	$thread = create('App\Thread');
-
-    	$reply = make('App\Reply');
-
-    	$this->post($thread->path().'/replies', $reply->toArray());
-
-    	$this->get($thread->path())
-    		->assertSee($reply->body);
+        $this->signIn();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply');
+        $this->post($thread->path() . '/replies', $reply->toArray());
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+    /** @test */
+    function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+        $this->post($thread->path() . '/replies', $reply->toArray())
+             ->assertSessionHasErrors('body');
     }
 }
