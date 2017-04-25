cf7c0c2c8594b1dd09e4b2fcba5465fe7de6b870
Frantiq90 <oscar.kosela@gmail.com>
2017-04-25 20:58:36 +0000

    Authorization With Policies

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,25 +4,27 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Forum threads</div>
-
-                <div class="panel-body">
-                    @foreach ($threads as $thread)
-                    <article>
+            @forelse ($threads as $thread)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
                         <div class="level">
-                            <h4 class="flex">
-                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                            </h4>
+                                <h4 class="flex">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                </h4>
 
-                            <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count ) }}</a>
-                        </div>
-                        <div class="body">{{ $thread->body }}</div>
-                    </article>
-                    <hr>
-                    @endforeach
+                                <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count ) }}</a>
+                            </div>
+                    </div>
+
+                    <div class="panel-body">
+                        <article>
+                            <div class="body">{{ $thread->body }}</div>
+                        </article>                    
                 </div>
             </div>
+            @empty
+                <p>There are no revelant results at this time.</p>
+            @endforelse
         </div>
     </div>
 </div>
