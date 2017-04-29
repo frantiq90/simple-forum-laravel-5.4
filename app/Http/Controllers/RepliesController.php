26eb04a9e4ad7c70f074d6c69327929af8a4c502
Frantiq90 <oscar.kosela@gmail.com>
2017-04-29 17:46:49 +0000

    reply can be deleted

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Thread;
+use App\Reply;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
@@ -22,5 +23,14 @@ class RepliesController extends Controller
     	]);
 
     	return back()->with('flash', 'Your reply has been left!');;
+    }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+        
+        $reply->delete();
+
+        return back();
     }
 }
